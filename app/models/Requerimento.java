e3a119101eb2befc1073bbabf63780e0569e4c4e
Davi Guimaraes <ddaavvii2001@gmail.com>
2019-08-20 23:14:19 -0300

    Mudança no modelo requerimento. Este commit faz parte de uma fase experimental.

--- app/models/Requerimento.java
+++ app/models/Requerimento.java
@@ -6,8 +6,9 @@ import play.db.jpa.Model;
 
 @Entity
 public class Requerimento extends Model{
-	public String aluno;
-	public String professor;
-	public String tipo;
+	public Long aluno_id;
+	public Long professor_id;
+	public Long id;
+	public Long tipo_id;
 	
 }
