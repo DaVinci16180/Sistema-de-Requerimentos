5790b4f1f55d05dfeadb9aded269c71e53a77ea8
Davi Guimaraes <ddaavvii2001@gmail.com>
2019-08-22 22:56:21 -0300

    correção no modelo requerimento

--- app/models/Requerimento.java
+++ app/models/Requerimento.java
@@ -6,9 +6,8 @@ import play.db.jpa.Model;
 
 @Entity
 public class Requerimento extends Model{
-	public Long aluno_id;
-	public Long professor_id;
-	public Long id;
-	public Long tipo_id;
+	public String aluno;
+	public String professor;
+	public String tipo;
 	
 }
